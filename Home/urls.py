cf822e73766c2cac248bba68bd9fa76430d5a78b
yaosichao0915 <yaosichao0915@gmail.com>
2017-08-02 13:20:38 +0800

    Initial commit

---
+++ Home/urls.py
@@ -0,0 +1,11 @@
+from django.conf.urls import url
+from Home import views
+from django.conf.urls.static import static
+from django.conf import settings
+
+
+app_name = 'Home'
+urlpatterns = [
+    url(r'^$', views.index, name='index'),
+    
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
